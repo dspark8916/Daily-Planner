c718680a575524671cad25a8661c80b5bf6cc3b9
Dimitar Micevski <dmicevski@hotmail.com>
2020-11-20 23:00:12 -0500

    edited function to show colors for time slots and updated html file

--- assets/script.js
+++ assets/script.js
@@ -8,64 +8,28 @@ $(document).ready(function(){
         $("#rightNow").text(todaysDate);
         
     $("#clear").click(function() {
-        // localStorage.clear();
-        // location.reload()
-        // refreshPlans();
 });
 
-var workDayHrs = ['9', '10', '11', '12', '1', '2', '3', '4', '5'];
+var workDayHrs = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
 
 var presentTime = moment().format("HH");
 
 $("#textInput").each(function() {
-    
-})
+    var timeSlot = parseInt($(this).prev().attr("data-hour"));
+    if (timeSlot == presentTime) {
+        $(this).addClass("present");
+    }
+    else if (timeSlot < presentTime) {
+        $(this).addClass("past");
+    }
+    else {
+        $(this).addClass("future");
+    }
+});
 
-function timeSlot() {
-    var taskTime = moment().hour();
-    console.log(timeSlot);
-    var presentTime = $('.row');
 
-    presentTime.each(function() {
-        var confirm = parseInt($(this).attr('id'))
-        if (confirm < 9) {
-            confirm += 12;
-        }
-        let time = $(this)[0];
-        if (taskTime === confirm) {
-            $(time).addClass('present')
-        } else if (taskTime > confirm) {
-            $(time).addClass('past')
-        } else if (taskTime < confirm) {
-            $(time).addClass('future')
-        }
-    });
-}
 
-timeSlot();
 
-// storage = window.localStorage;
-// function saveToStorage(key, value) {
-//     localStorage.setItem(key, value);
-// }
-
-// function getStorage(key) {
-//     let item = localStorage.getItem(key);
-//     if (item == null) return "";
-//     return item;
-// }
-
-// let plans = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm'];
-
-// function refreshPlans() {
-//     for (text of plans) {
-//         let item = getStorage(text);
-//         if (item != null) {
-//             let inputArea = text + "text";
-//             document.getElementById(inputArea).value = item;
-//         }
-//     }
-// }
 
 
 
