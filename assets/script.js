a6b1702c8bdffb7501597642e8ef52807d2285e8
Dimitar Micevski <dmicevski@hotmail.com>
2020-11-21 00:46:43 -0500

    finalized function to save user input to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,40 @@
-var todaysDate = moment().format('dddd, MMMM Do, YYYY');
-$("#todayIs").text(todaysDate);
+$(document).ready(function () {
+
+    var todaysDate = moment().format('dddd, MMMM Do, YYYY');
+    $("#todayIs").text(todaysDate);
 
 
-var todaysDate = moment().format('h:mm a');
-$("#rightNow").text(todaysDate);
+    var todaysDate = moment().format('h:mm a');
+    $("#rightNow").text(todaysDate);
 
-$("#clear").click(function() {
-    localStorage.clear();
+    $("#clear").click(function () {
+        localStorage.clear();
+    });
+
+    var workDayHrs = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+    var presentTime = moment().format("HH");
+
+    $(".textInput").each(function () {
+        var timeSlot = parseInt($(this).prev().attr("data-hour"));
+        if (timeSlot == presentTime) {
+            $(this).addClass("present");
+        }
+        else if (timeSlot < presentTime) {
+            $(this).addClass("past");
+        }
+        else {
+            $(this).addClass("future");
+        }
+    });
+
+    $(".saveBtn").click(function () {
+        var userInput = $(this).siblings(".textInput").val();
+        var workHr = $(this).attr('id');
+        localStorage.setItem(workHr, userInput);
+    });
+
 });
-
-var workDayHrs = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-
-var presentTime = moment().format("HH");
-
-$(".textInput").each(function() {
-    var timeSlot = parseInt($(this).prev().attr("data-hour"));
-    if (timeSlot == presentTime) {
-        $(this).addClass("present");
-    }
-    else if (timeSlot < presentTime) {
-        $(this).addClass("past");
-    }
-    else {
-        $(this).addClass("future");
-    }
-});
-
-$(".saveBtn").on("click", function() {
-    var workHr = $("this").attr("id");
-    var userInput = $("this").siblings(".textInput").val();
-
-    localStorage.setItem(workHr, userInput);
-});
-
 
 
 
